2e8eef4f549b6950b3751da96dd38afa40e5eab9
Joey <joey@lineageos.org>
2018-02-04 11:36:32 +0100

    Trebuchet: allow disabling workspace edit
    [BadDaemon: Adapt for S changes]
    
    Change-Id: I503e19cbc512eac0e4a8c8bccc16a6ccc0e805da
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -932,4 +932,9 @@ public final class Utilities {
         }
         return options;
     }
+
+    public static boolean isWorkspaceEditAllowed(Context context) {
+        SharedPreferences prefs = getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(InvariantDeviceProfile.KEY_WORKSPACE_EDIT, true);
+    }
 }
